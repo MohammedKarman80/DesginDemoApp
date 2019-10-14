29401d8908ccdcd422d1d800107a82d488117ca0
Mohammed Karman <m-t0707@hotmail.com>
2019-10-14 15:19:47 +0300

    mainActivity

--- app/src/main/java/com/example/desgindemoapp/MainActivity.kt
+++ app/src/main/java/com/example/desgindemoapp/MainActivity.kt
@@ -2,11 +2,64 @@ package com.example.desgindemoapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        replaceFragment(SkillsFragment())
+    }
+
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_skills -> {
+                replaceFragment(SkillsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_harbor -> {
+                replaceFragment(SkillsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_balance -> {
+                replaceFragment(SkillsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_calender -> {
+                replaceFragment(SkillsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add -> {
+                replaceFragment(SkillsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frame_content, fragment)
+        fragmentTransaction.addToBackStack("null")
+        fragmentTransaction.commit()
+
+    }
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 1)
+            supportFragmentManager.popBackStack()
+        else
+            finish()
     }
 }
---
+++ app/src/main/java/com/example/desgindemoapp/SkillsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.desgindemoapp
+
+import android.support.v4.app.Fragment
+
+class SkillsFragment : Fragment() {
+}
