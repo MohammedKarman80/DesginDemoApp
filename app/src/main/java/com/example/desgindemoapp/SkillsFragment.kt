ff02e8f64745b096189ada00b248ecba2ff78ce1
Mohammed Karman <m-t0707@hotmail.com>
2019-10-15 18:06:23 +0300

    minor changes in tablayout

--- app/src/main/java/com/example/desgindemoapp/SkillsFragment.kt
+++ app/src/main/java/com/example/desgindemoapp/SkillsFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.text.Spannable
 import android.text.SpannableString
@@ -12,6 +13,7 @@ import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import kotlinx.android.synthetic.main.skills_fragment.*
 import kotlinx.android.synthetic.main.skills_fragment.view.*
 import kotlinx.android.synthetic.main.skills_fragment.view.button
@@ -22,11 +24,8 @@ class SkillsFragment : Fragment(),View.OnClickListener {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view =inflater.inflate(R.layout.skills_fragment,container,false)
         var text :  String= view.span_text.text.toString()
-
+        val button1 : TabLayout = view.findViewById(R.id.button)
         view.span_text.setText( setBoldSpannable(text))
-        view.button.setOnClickListener(this)
-        view.button2.setOnClickListener(this)
-        view.button3.setOnClickListener(this)
         view.card_view1.setOnClickListener(this)
         view.card_view2.setOnClickListener(this)
         view.card_view3.setOnClickListener(this)
@@ -36,21 +35,6 @@ class SkillsFragment : Fragment(),View.OnClickListener {
 
     override fun onClick(v: View?) {
       when(v!!.id){
-          R.id.button -> {
-             button.setBackgroundResource(R.drawable.blue_custome_button)
-              button2.setBackgroundResource(R.drawable.custome_button)
-              button3.setBackgroundResource(R.drawable.custome_button)
-          }
-          R.id.button2 ->{
-              button2.setBackgroundResource(R.drawable.blue_custome_button)
-              button.setBackgroundResource(R.drawable.custome_button)
-              button3.setBackgroundResource(R.drawable.custome_button)
-          }
-          R.id.button3 ->{
-              button3.setBackgroundResource(R.drawable.blue_custome_button)
-              button.setBackgroundResource(R.drawable.custome_button)
-              button2.setBackgroundResource(R.drawable.custome_button)
-          }
           R.id.card_view1, R.id.card_view2, R.id.card_view3->{
               val intent = Intent (getActivity(), DetailsSkillsActivity::class.java)
               getActivity()!!.startActivity(intent)
@@ -59,6 +43,7 @@ class SkillsFragment : Fragment(),View.OnClickListener {
 
       }
     }
+
 
     fun setBoldSpannable(myText: String): SpannableString {
         val spannableContent = SpannableString(myText)
